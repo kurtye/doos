0bde55b58ebca7fbf82f7b77cbb3e67114766ffc
kurtye <kurtyebsb@gmail.com>
2017-08-09 17:47:42 -0300

    Conexão Firebase

---
+++ www/js/config.js
@@ -0,0 +1,13 @@
+angular.module('app.configs', [])
+
+  .constant("CONFIG", {
+    "FIREBASE_API": 'AIzaSyAMje_G2LZM1kylhp2i3gc2ymL3v1_xr4k',
+    "FIREBASE_AUTH_DOMAIN": 'doos-seu-evento.firebaseapp.com',
+    "FIREBASE_DB_URL": 'https://doos-seu-evento.firebaseio.com',
+    "FIREBASE_STORAGE": 'doos-seu-evento.appspot.com',
+    "FIREBASE_PROJECT_ID":'doos-seu-evento',
+    "MESSAGE_SENDER_ID": '641161137388'
+  });
+
+
+
