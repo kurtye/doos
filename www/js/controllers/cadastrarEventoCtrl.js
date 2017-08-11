cb1f45d52d06f431289c22656162eb90565f2627
kurtye <kurtyebsb@gmail.com>
2017-08-11 15:17:18 -0300

    Cadastrar eventos

--- www/js/controllers/cadastrarEventoCtrl.js
+++ www/js/controllers/cadastrarEventoCtrl.js
@@ -1,5 +1,23 @@
 angular.module('cadastrarEventoCtrls', []).controller('cadastrarEventoCtrl', ['$scope', '$stateParams',
     function ($scope, $stateParams) {
 
+      $scope.addEvento = function (evento) {
+
+        firebase.database().ref('eventos/').push(evento);
+
+
+        $state.go("tab.principal");
+
+        swal({
+          title: " Cadastrado com Sucesso",
+          text: "Boa Sorte",
+          type: "success",
+          showConfirmlButton: false,
+          timer: 2000
+        });
+
+        $scope.evento = {};
+      };
+
 
     }])
