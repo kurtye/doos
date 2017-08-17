8eb876a6ab14963f65bcb0d4659a3ca17974f0c7
kurtye <kurtyebsb@gmail.com>
2017-08-16 22:55:44 -0300

    Ajustes

--- www/js/controllers/cadastrarEventoCtrl.js
+++ www/js/controllers/cadastrarEventoCtrl.js
@@ -1,44 +1,51 @@
 angular.module('cadastrarEventoCtrls', []).controller('cadastrarEventoCtrl', ['$scope', '$state', '$stateParams', 'eventoService',
-    function ($scope, $state, $stateParams, eventoService) {
+    '$cordovaImagePicker',
+    function ($scope, $state, $stateParams, eventoService, $cordovaImagePicker) {
+
+
+      $scope.imgURL = document.getElementById("files");
+
+      //INICIO DO UPLOAD
+      window.previewFile = function previewFile() {
+        //var storage = firebase.storage();
+
+        var file = document.getElementById("files").files[0];
+
+        var id = user.userId;
+
+        var storageRef = firebase.storage().ref();
+
+        //dynamically set reference to the file name
+        var thisRef = storageRef.child('images/' + id + '/' + file.name);
+
+
+        //put request upload file to firebase storage
+        thisRef.put(file).then(function (snapshot) {
+          var url = snapshot.downloadURL;
+          $scope.instaURL = url;
+
+          document.getElementById('linkbox').innerHTML = '<img src="' + url + '" style="width: 100%;" />';
+
+          $rootScope.instaURL = url;
+          $scope.instaPet = {
+            "usuario": user.userId,
+            "nomeUsuario": user.displayName,
+            "email": user.email,
+            "fotoUsuario": user.imageUrl,
+            "instaFoto": url
+          };
+
+        });
+
+        //get request to get URL for uploaded file
+        thisRef.getDownloadURL().then(function (url) {
+
+        })
+
+      };//FIM UPLOAD DE IMAGEM
+
 
         $scope.addEvento = function (evento) {
-
-
-            //scope.imgURL = document.getElementById("files");
-            ///INICIO DO UPLOAD
-            //indow.previewFile = function previewFile() {
-            //   var storage = firebase.storage();
-
-            //   var file = document.getElementById("files").files[0];
-            //   console.log(file);
-
-            //   var storageRef = firebase.storage().ref();
-
-            //   //dynamically set reference to the file name
-            //   if (key) {
-            //       var thisRef = storageRef.child('images/eventos/' + key + '/' + file.name);
-            //   }else {
-            //       var petKey = PetService.getNewPetKey().key;
-            //       $scope.key = petKey;
-            //       console.log(petKey);
-
-            //       var thisRef = storageRef.child('images/adocao/' + petKey + '/' + file.name);
-
-            //   }
-
-
-            //   //put request upload file to firebase storage
-            //   thisRef.put(file).then(function (snapshot) {
-            //       var url = snapshot.downloadURL;
-
-
-            //       document.getElementById('linkbox').innerHTML = '<img src="' + url + '" style=" width: 100px; " />';
-
-
-            //       $scope.pet.imgURL = url;
-            //       console.log(url);
-            //   });
-
 
             evento.id = eventoService.getNewKey().key;
 
